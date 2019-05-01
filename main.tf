04fe7f80b6752e42dce6987ccc8dfad4e5bb294f
Laurent <laurent.lesle@microsoft.com>
2019-05-01 12:49:19 +0800

    Fix variable typo

--- main.tf
+++ main.tf
@@ -11,13 +11,13 @@ locals {
 
 resource "azurerm_log_analytics_solution" "container_insights" {
   count                 = "${length(local.solution_list)}"
-  solution_name         = "${element(local.solution_list, count.index)}"
+  solution_name         = "${local.solution_list}"
   location              = "${var.location}"
   resource_group_name   = "${var.resource_group_name}"
   workspace_resource_id = "${azurerm_log_analytics_workspace.log.id}"
   workspace_name        = "${azurerm_log_analytics_workspace.log.name}"
 
  
-  plan  = "${var.solution_plan_map["${element(var.solutions, count.index)}"]}"
+  plan  = "${var.solution_plan_map["${element(local.solution_list, count.index)}"]}"
     
 }
