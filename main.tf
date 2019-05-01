e5e1ff5742edbe1a529b421d5ff72598c5985b38
Laurent <laurent.lesle@microsoft.com>
2019-05-01 12:46:09 +0800

    Removing solution_name

--- main.tf
+++ main.tf
@@ -5,14 +5,19 @@ resource "azurerm_log_analytics_workspace" "log" {
   sku                 = "PerGB2018"
 }
 
+locals {
+    solution_list = "${keys(var.solution_plan_map)}"
+}
+
 resource "azurerm_log_analytics_solution" "container_insights" {
-  count                 = "${length(var.solutions)}"
-  solution_name         = "${element(var.solutions, count.index)}"
+  count                 = "${length(local.solution_list)}"
+  solution_name         = "${element(local.solution_list, count.index)}"
   location              = "${var.location}"
   resource_group_name   = "${var.resource_group_name}"
   workspace_resource_id = "${azurerm_log_analytics_workspace.log.id}"
   workspace_name        = "${azurerm_log_analytics_workspace.log.name}"
 
+ 
   plan  = "${var.solution_plan_map["${element(var.solutions, count.index)}"]}"
     
 }
--- variables.tf
+++ variables.tf
@@ -15,20 +15,14 @@ variable "name" {
   
 }
 
-variable "solutions" {
-    description = "List of the log solutions to create"
-    type        = "list"
-}
-
-
 variable "solution_plan_map" {
     type = "map"
 }
 
 # Map structure:
 # solution_plan_map = {
-#     ContainerInsights {
+#     ContainerInsights = [{
 #         publisher = "Microsoft"
 #         product   = "OMSGallery/ContainerInsights"
-#     }
+#     }]
 # }
