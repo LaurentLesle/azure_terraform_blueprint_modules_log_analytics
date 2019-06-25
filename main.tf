1b3fe06aab96f9895cb60b6381e050f030b23b66
lolorol <LaurentLesle@users.noreply.github.com>
2019-06-25 15:17:30 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ locals {
   solution_list = keys(var.solution_plan_map)
 }
 
-resource "azurerm_log_analytics_solution" "container_insights" {
+resource "azurerm_log_analytics_solution" "solution" {
   count                 = length(local.solution_list)
   solution_name         = element(local.solution_list, count.index)
   location              = var.location
@@ -17,17 +17,6 @@ resource "azurerm_log_analytics_solution" "container_insights" {
   workspace_resource_id = azurerm_log_analytics_workspace.log.id
   workspace_name        = azurerm_log_analytics_workspace.log.name
 
-  dynamic "plan" {
-    for_each = var.solution_plan_map
-    content {
-      # TF-UPGRADE-TODO: The automatic upgrade tool can't predict
-      # which keys might be set in maps assigned here, so it has
-      # produced a comprehensive set here. Consider simplifying
-      # this after confirming which keys can be set in practice.
-
-      product        = plan.value.product
-      publisher      = plan.value.publisher
-    }
-  }
+  plan = "${var.solution_plan_map["${element(local.solution_list, count.index)}"]}"
 }
 
